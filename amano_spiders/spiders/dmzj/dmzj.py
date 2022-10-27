86df72a04e31d43f32ff5638c66f1067d7a3c372
卫峰 <im.wf@outlook.com>
2022-10-27 11:16:25 +0800

    feature：实现获取评分逻辑，完善数据库模型

--- amano_spiders/spiders/dmzj/dmzj.py
+++ amano_spiders/spiders/dmzj/dmzj.py
@@ -1,12 +1,9 @@
 import datetime
-import os
+import json
 import re
 
+import requests
 import scrapy
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from sqlalchemy import Date
-from execjs import execjs
 
 from amano_spiders.spiders.dmzj.model.comic import ComicItem
 
@@ -18,7 +15,7 @@ class DmzjSpider(scrapy.Spider):
         self.base_url = 'https://www.dmzj.com'
         self.allowed_domains = ['dmzj.com']
         self.start_urls = ['https://www.dmzj.com/category/1-0-0-0-0-0-1.html']
-        self.rex = re.compile('//v3api.dmzj.com/comic/hot_sub_num/\d+.json')
+        self.rex = re.compile('var comic_id = \'\d+\'')
         # self.chrome_options = Options()
         # self.chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
         # self.chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
@@ -48,9 +45,8 @@ class DmzjSpider(scrapy.Spider):
         next_page_url = self.base_url + next_page_ele[0].attrib['href']
         yield scrapy.Request(url=next_page_url)
 
+    #进详情页爬取
     def detail_page(self, response):
-        # https://i.dmzj.com/ajax/getScoreInfo?callback=success_jsonpCallback_201509221731&comic_id=20844&_=1666798979647
-        self.rex.findall(response.text)[0]
         item = response.meta['item']
         info_eles = response.xpath('//ul[@class="comic_deCon_liO"]/li/text()')
         item["describe"] = response.xpath('//p[@class="comic_deCon_d"]/text()').extract()[0]
@@ -58,12 +54,22 @@ class DmzjSpider(scrapy.Spider):
             item["author"] = info_eles[0].extract()[3:-1]
         if info_eles[1].extract():
             state = info_eles[1].extract()
-            item['state'] = (state == '连载中') if 0 else 1
+            item['state'] = 0 if (state == '状态：连载中') else 1
         if info_eles[2].extract():
             item["category"] = info_eles[2].extract()[3:]
         if info_eles[3].extract():
-            item["tags"] = info_eles[3].extract()[3:]
+            item["tags"] = info_eles[3].extract()[3:].split(' | ')
         update_time_ele = response.xpath('//span[@class="zj_list_head_dat"]/text()').extract()[0][7:-2]
         item['update_time'] = datetime.datetime.strptime(update_time_ele, "%Y-%m-%d")
-        item['score'] = response.xpath('//em[@id="scorData"]/text()').extract()[0]
+        comic_id = self.rex.findall(response.text)[0][16:-1]
+        item['score'] = self.get_score(comic_id)
+        item['comic_id'] = comic_id
         yield item
+
+    # 解析comic_id通过请求获取评分
+    def get_score(self, comic_id):
+        res = requests.get(
+            url="https://i.dmzj.com/ajax/getScoreInfo?callback=success_jsonpCallback_201509221731&comic_id={}&_=1666798979647".format(
+                comic_id))
+        recore_json = json.loads(re.compile('".+"').findall(res.text)[0][1: -1].replace('\\', ''))
+        return recore_json['show_points']
--- amano_spiders/spiders/dmzj/model/comic.py
+++ amano_spiders/spiders/dmzj/model/comic.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, MetaData, String, Text, Table, Date
+from sqlalchemy import Column, Integer, MetaData, String, Text, Table, Date, Float, JSON
 from amano_spiders.spiders.config.dbconfig import get_engine
 from scrapy_sqlitem import SqlItem
 from scrapy_sqlitem.sqlitem import SqlAlchemyItemMeta
@@ -9,14 +9,15 @@ metadata = MetaData(get_engine())
 class ComicItem(SqlItem, metaclass=SqlAlchemyItemMeta):
     sqlmodel = Table("comic", metadata,
                      Column('id', Integer, primary_key=True, autoincrement=True),
+                     Column('comic_id', Integer),
                      Column('comic_name', String(50)),
                      Column('detail_url', String(256)),
                      Column('cover_url', String(256)),
                      Column('author', String(20)),
                      Column('category', String(50)),
                      Column('state', Integer),
-                     Column('score', Integer),
-                     Column("tags", Text),
+                     Column('score', Float),
+                     Column("tags", JSON),
                      Column('describe', Text),
                      Column("update_time", Date),
                      )
